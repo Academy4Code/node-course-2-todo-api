7b8e886accd2c8e6c80d7d38227c2eff56bc8799
ravindra Kumar <ravi16oct1989@gmail.com>
2018-05-23 09:57:02 +0530

    Add update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,45 @@
+// const MongoClient= require('mongodb').MongoClient;
+const  {MongoClient, ObjectID}= require('mongodb');
+
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp',(err,db)=>{
+  if (err) {
+    console.log('Unable to connect to mongodb server');
+  }
+    console.log('Connected to MongoDB Server');
+
+db.collection('Users').findOneAndUpdate({
+  _id:new ObjectID('5afd0cdb47cc0828c8dbfc50')
+},{
+  $set:
+  {
+    name:'Ravindra Kumar'
+  },
+   $inc:
+    {
+       "age": 1
+    }
+
+},{
+  returnOriginal :true
+}).then((result)=>{
+  console.log(result);
+});
+
+
+// db.collection('Todos').findOneAndUpdate({
+//   _id: new ObjectID('5afbbffd2465381fdc20b79a')
+// },
+// {
+//   $set:
+//   {
+//     completed:true
+//   }
+// },
+// {
+//   returnNewDocument :false
+// }).then((result)=>{
+//   console.log(result);
+// });
+
+});
